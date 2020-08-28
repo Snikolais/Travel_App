ec6d3e5fd68cd2635b6fc9ce2840a75803b1754b
Nikolai <salmin.n@outlook.com>
2020-08-28 22:56:00 +0300

    Initial Commit

---
+++ Travel_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Travel_App
+//
+//  Created by Николай on 28.08.2020.
+//  Copyright © 2020 Nikolai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
